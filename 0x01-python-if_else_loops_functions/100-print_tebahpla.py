0cd1aa1477dff9eafe51d059b6bb9273b5034e83
root <aurelle.adjahou@gmail.com>
2022-05-31 19:30:47 -0700

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for l in range(122, 96, -2):
-    print("{:s}{:s}".format(chr(l), chr(l - 33)), end="")
+
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0
