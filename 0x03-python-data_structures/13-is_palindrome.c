2fab283ea48a2a592caf76af74e2beb22a82a351
root <aurelle.adjahou@gmail.com>
2022-06-06 12:11:16 -0700

    13-is_palindrome.c

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - check if a linked list is a palindrome
+ * @head: pointer to head of list
+ * Return: 0 or 1
+ */
+int is_palindrome(listint_t **head)
+{
+	const listint_t *current;
+	const listint_t *tail;
+	unsigned int n = 0;
+	return (n);
+}
