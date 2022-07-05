1491b5fde050f4b04bdc4873662e8add51516ca7
root <aurelle.adjahou@gmail.com>
2022-07-05 15:24:16 -0700

    100-append_after.py

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 """append after module"""
+
+
 def append_after(filename="", search_string="", new_string=""):
+
     """append a text after a substring line"""
 
     with open(filename, 'r') as f:
+
         content = f.readlines()
         for (index, line) in enumerate(content):
             if line.find(search_string) != -1:
